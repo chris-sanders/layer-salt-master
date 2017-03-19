f2f77207e23e0aa7e688e0fe2e8164bd52743f60
Chris Sanders <sanders.chris@gmail.com>
2017-03-19 17:24:34 -0500

    installs now

--- reactive/salt_master.py
+++ reactive/salt_master.py
@@ -1,8 +1,14 @@
 from charms.reactive import when, when_not, set_state
+from charmhelpers.core.hookenv import status_set
+from charmhelpers.fetch import apt_install
 import subprocess
 
 @when_not('salt-master.installed')
 @when('salt-common.installed')
 def install_layer_salt_master():
-    subprocess.call(['sudo apt-get install salt-master -y'],shell=True)
+    status_set('maintenance','installing salt-master')
+    apt_install('salt-master')
+    #subprocess.call(['sudo apt-get install salt-master -y'],shell=True)
+
+    status_set('active','')
     set_state('salt-master.installed')
