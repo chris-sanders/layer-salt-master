e6342b8cfc237b2ff7c9db96cd9c220c39477a79
Chris Sanders <sanders.chris@gmail.com>
2017-03-19 17:16:36 -0500

    corrected install command

--- reactive/salt_master.py
+++ reactive/salt_master.py
@@ -4,5 +4,5 @@ import subprocess
 @when_not('salt-master.installed')
 @when('salt-common.installed')
 def install_layer_salt_master():
-    subprocess.call(['sudo','apt install','salt-master'])
+    subprocess.call(['sudo apt-get install salt-master -y'],shell=True)
     set_state('salt-master.installed')
