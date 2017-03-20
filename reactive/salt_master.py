8c752719ae5f0a3d7c9d71c64b978c8b76b844b7
Chris Sanders <sanders.chris@gmail.com>
2017-03-19 20:21:34 -0500

    rsa generation appears to be working

--- reactive/salt_master.py
+++ reactive/salt_master.py
@@ -1,14 +1,42 @@
 from charms.reactive import when, when_not, set_state
 from charmhelpers.core.hookenv import status_set
 from charmhelpers.fetch import apt_install
-import subprocess
+from cryptography.hazmat.primitives import serialization as crypto_serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.backends import default_backend as crypto_default_backend
+import os
 
 @when_not('salt-master.installed')
 @when('salt-common.installed')
-def install_layer_salt_master():
+def install_salt_master():
     status_set('maintenance','installing salt-master')
     apt_install('salt-master')
-    #subprocess.call(['sudo apt-get install salt-master -y'],shell=True)
-
     status_set('active','')
     set_state('salt-master.installed')
+
+@when_not('ssh-key.generated')
+def generate_ssh_key(): 
+    key = rsa.generate_private_key(
+        backend=crypto_default_backend(),
+        public_exponent=65537,
+        key_size=4096)
+    private_key = key.private_bytes(
+        crypto_serialization.Encoding.PEM,
+        crypto_serialization.PrivateFormat.PKCS8,
+        crypto_serialization.NoEncryption())
+    public_key = key.public_key().public_bytes(
+        crypto_serialization.Encoding.OpenSSH,
+        crypto_serialization.PublicFormat.OpenSSH)
+    keypath = './rsa'
+    try:
+        os.mkdir(keypath)
+    except OSError as e:
+        if e.errno is 17:
+          pass
+    with open(keypath+'/id_rsa','wb') as file:
+        file.write(private_key)
+    with open(keypath+'/id_rsa.pub','wb') as file:
+        file.write(public_key)
+    print("is_rsa.pub: {}".format(repr(public_key)))
+    set_state('ssh-key.generated')
+
