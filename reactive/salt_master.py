d9753b2325affd7315853cb9bba9d7482fdf1bb1
Chris Sanders <sanders.chris@gmail.com>
2017-03-21 14:00:50 -0500

    If no repo is provided init one in /srv

--- reactive/salt_master.py
+++ reactive/salt_master.py
@@ -1,6 +1,6 @@
 from charms.reactive import when, when_not, set_state
 from charmhelpers.core import hookenv
-from charmhelpers.core.hookenv import status_set, resource_get
+from charmhelpers.core.hookenv import status_set, resource_get, log
 from charmhelpers.core.host import chownr
 from charmhelpers.fetch import apt_install
 from cryptography.hazmat.primitives import serialization as crypto_serialization
@@ -15,12 +15,12 @@ import shutil
 def install_salt_master():
     status_set('maintenance','installing salt-master')
     apt_install('salt-master')
-    status_set('active','')
     set_state('salt-master.installed')
 
 @when_not('ssh-key.generated')
 @when('salt-master.installed')
 def generate_ssh_key(): 
+    status_set('maintenance','setting rsa keys')
     keypath = './rsa'
     privateKey = keypath+'/id_rsa'
     publicKey = keypath+'/id_rsa.pub'
@@ -66,15 +66,33 @@ def generate_ssh_key():
 
 @when('ssh-key.generated')
 @when_not('git-cloned')
+@when_not('git-created')
+def setup_repository():
+    config = hookenv.config()
+    if config['git-repo'] is not "":
+      pull_repository()
+    else:
+      create_repository()
+    chownr('/srv',owner='ubuntu',group='ubuntu')
+    status_set('active','')
+
+def create_repository():
+    status_set('maintenance','creating salt repository')
+    os.makedirs('/srv/salt/salt-formulas')
+    os.makedirs('/srv/pillar')
+    subprocess.check_call(["git init /srv"],shell=True)
+    set_state('git-created')
+
 def pull_repository():
+    status_set('maintenance','pulling salt repository')
     config = hookenv.config()
     try:
         os.environ["GIT_SSH_COMMAND"] = "ssh -i $JUJU_CHARM_DIR/rsa/id_rsa -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
         subprocess.check_call(["git clone {} /srv".format(config['git-repo'])],shell=True)     
     except Exception as e:
-        print("Unable to pull git repository")
+        status_set('maintenance','repository not available')
+        log("Unable to pull git repository")
         return
-    chownr('/srv',owner='ubuntu',group='ubuntu')
     set_state('git-cloned')
 
 
